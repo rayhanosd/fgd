9ca7ccc2d02e63a5dae42f091e21ae52a1fac243
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-06-03 22:01:55 +0600

    ljh

---
+++ sasasass.c
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+     int main ()
+     {
+         int n;
+         double x;
+         x=20.4;
+         n=(int)x;
+         printf("%d\n",n);
+         printf("%lf\n",x);
+
+         return 0;
+     }
